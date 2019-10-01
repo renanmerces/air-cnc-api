0b513f114684d6c4d072734b512a75dc5444d607
Anunciacao <renan.anunciacao@capgemini.com>
2019-10-01 15:02:46 -0300

    First commit

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const sessionController = require('./controllers/SessionController')
+
+const routes = express.Router()
+
+routes.post('/sessions', sessionController.store)
+
+module.exports = routes
